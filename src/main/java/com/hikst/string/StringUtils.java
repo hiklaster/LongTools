0ce145809e6c41c3e7824bee1086bb112157db4d
john.xie <john.xie@gmail.com>
2024-09-05 17:53:54 +0800

    完成M-R任务，更新文档

--- src/main/java/com/hikst/string/StringUtils.java
+++ src/main/java/com/hikst/string/StringUtils.java
@@ -11,9 +11,13 @@ import java.util.regex.Pattern;
  * @date 2024-09-01 20:39
  */
 public class StringUtils {
+
+    private static final char[] UPPERCASE_LETTERS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public static final char[] LOWERCASE_LETTERS = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     /**
      * 获取字符串的长度
-     * @param ch
+     * @param ch 需查询的字符序列
      * @return 字符串的长度，如果不是String类型，返回-1
      */
     public static int getLength(CharSequence ch){
@@ -25,8 +29,8 @@ public class StringUtils {
 
     /**
      * 获取字符串中一个字符的角标位
-     * @param str
-     * @param ch
+     * @param str 原始字符串
+     * @param ch 需查询的字符序列
      * @return 字符角标位，如不存在或str为null或字符串长度为0时或字符为null时返回-1
      */
     public static int getIndex(String str,Character ch){
@@ -45,9 +49,9 @@ public class StringUtils {
 
     /**
      * 在字符串中查找能匹配上的字符串开始角标
-     * @param str
-     * @param ch
-     * @return
+     * @param str 原始字符串
+     * @param ch 需查询的字符串
+     * @return 查询字符串的角标值
      */
     public static int getIndex(String str,String ch){
         if(str == null || str.length() == 0 || ch == null || ch.length() == 0) return -1;
@@ -72,9 +76,9 @@ public class StringUtils {
 
     /**
      * 在字符串中查找能匹配上的字符串开始角标，使用正则实现
-     * @param str
-     * @param ch
-     * @return
+     * @param str 原始字符串
+     * @param ch 查找的字符或正则表达式
+     * @return 查询到的字符串的角标值
      */
     public static int getIndexOfPattern(String str,String ch){
         if(str == null || str.length() == 0 || ch == null || ch.length() == 0) return -1;
@@ -88,8 +92,8 @@ public class StringUtils {
 
     /**
      * 获取字符串中某一角标位的字符
-     * @param str
-     * @param index
+     * @param str 原始字符串
+     * @param index 查找的字符索引值
      * @return 角标位的字符
      */
     public static String getStringOfIndex(String str,int index){
@@ -102,9 +106,9 @@ public class StringUtils {
 
     /**
      * 获取字符串中从start至end角标位的字符串
-     * @param str
-     * @param start
-     * @param end
+     * @param str 原始字符串
+     * @param start 开始角标
+     * @param end 结束角标
      * @return 对应角标位的字符串
      */
     public static String getStringOfIndex(String str,int start,int end){
@@ -342,5 +346,143 @@ public class StringUtils {
         char[] result = (char[])v.get(str);
         return result;
         // 最简单方式 return str.toCharArray()
+    }
+
+    /**
+     * 替换字符串中的内容
+     * @param str 需替换的字符串
+     * @param src 旧字符串
+     * @param desc 新字符串
+     * @return 替换后的字符串
+     */
+    public static String replace(String str,String src,String desc){
+        return str.replace(src,desc);
+    }
+
+    /**
+     * 替换第一个匹配上的字符串，支持正则表达式
+     * @param str 需替换的字符串
+     * @param oldStr 旧字符串
+     * @param newStr 新字符串
+     * @return 替换后的字符串
+     */
+    public static String replaceFirst(String str,String oldStr,String newStr){
+        return str.replaceFirst(oldStr,newStr);
+    }
+    /**
+     * 替换字符串中的内容，支持正则表达式
+     * @param str 需替换的字符串
+     * @param oldStr 旧字符串或正则表达式
+     * @param newStr 新字符串
+     * @return 替换后的字符串
+     */
+    public static String replaceAll(String str,String oldStr,String newStr){
+        return str.replaceAll(oldStr,newStr);
+    }
+
+    /**
+     * 判断传入的字符是否是26个大写字母
+     * @param ch 传入字符
+     * @return 判断结果
+     */
+    private static boolean isUpperCase(char ch){
+        return ch >= 'A' && ch <= 'Z';
+    }
+
+    /**
+     * 判断传入的字符是否是26个小写字母
+     * @param ch 传入的字符
+     * @return 判断的结果
+     */
+    private static boolean isLowerCase(char ch){
+        return ch >= 'a' && ch <= 'z';
+    }
+
+    /**
+     * 将字符串转为大写，只支持26个英文字母
+     * @param str 需转换的字符串
+     * @return 转换好的字符串
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public static String toUpperCase(String str) throws NoSuchFieldException, IllegalAccessException {
+        char[] chs = toCharArray(str);
+        for (int i = 0; i < chs.length; i++) {
+            if(isLowerCase(chs[i])){
+                chs[i] -= 32;
+            }
+        }
+        return valueOf(chs);
+    }
+
+    /**
+     * 将字符串转为小写，只支持26个英文字母
+     * @param str 需转换的字符串
+     * @return 转换好的字符串
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public static String toLowerCase(String str) throws NoSuchFieldException, IllegalAccessException {
+        char[] chs = toCharArray(str);
+        for (int i = 0; i < chs.length; i++) {
+            if(isUpperCase(chs[i])) chs[i] += 32;
+        }
+        return valueOf(chs);
+    }
+
+    /**
+     * 去除字符串两端的空白字符
+     * @param str 需被处理的字符串
+     * @return 去除两端空白字符后的字符串
+     */
+    public static String strip(String str){
+        char[] chs = str.toCharArray();
+        int startIndex = -1,endIndex = -1;
+        //思路 正反循环，记录角标，开始与结束角标，只返回对应角标范围的字符串
+        for (int i = 0; i < chs.length; i++) {
+            if(!Character.isWhitespace(chs[i])){
+                startIndex = i;
+                break;
+            }
+        }
+        for (int i = chs.length - 1; i >= 0; i--) {
+            if(!Character.isWhitespace(chs[i])){
+                endIndex = i;
+                break;
+            }
+        }
+        return str.substring(startIndex,endIndex + 1);
+    }
+
+    /**
+     * 将字符串根据正则表达式切割为字符串数组
+     * @param str 需切割的字符串
+     * @param regex 正则表达式字符串
+     * @return 切割后的字符串数组
+     */
+    public static String[] split(String str,String regex){
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.split(str);
+    }
+
+    /**
+     * 截取并获取字符串子串
+     * @param str 被截取的字符串
+     * @param beginIndex 开始角标
+     * @return 截取后的子字符串
+     */
+    public static String subString(String str,int beginIndex){
+        return str.substring(beginIndex);
+    }
+
+    /**
+     * 截取并获得字符串子串
+     * @param str 被截取的字符串
+     * @param beginIndex 开始角标
+     * @param endIndex 结束角标
+     * @return 截取后的子串
+     */
+    public static String subString(String str,int beginIndex,int endIndex){
+        return str.substring(beginIndex,endIndex);
     }
 }
